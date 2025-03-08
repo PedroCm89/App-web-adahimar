33ca3a1514fcbcc9eb6105a1fb55a5c33695881d
pedro <pedro.cm.1989@gmail.com>
2025-03-08 23:14:28 +0100

    footer y config

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  currentYear: number = new Date().getFullYear();
+
+
+  socialLinks = [
+    { icon: 'uil uil-instagram', url: 'https://www.instagram.com/adahimartdah/' },
+    { icon: 'uil uil-facebook', url: 'https://www.facebook.com/Adahimar.tdah/?locale=es_ES' },
+    { icon: 'uil uil-twitter-alt', url: 'https://x.com/adahimar_tdah' }
+  ];
+  /*sponsorLogos = [
+    'assets/logo_adahimar.svg',
+    'assets/logo_ayuntamiento.jpg'
+  ];*/
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ export default {
     "./src/**/*.{html,ts}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#02B0E1",     // Azul
+        secondary: "#FCDE35",   // Amarillo
+        tertiary: "#FF8000",      // Naranja
+        neutral: "#59595C",     // Gris
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'], // Fuente para párrafos y elementos generales
+        serif: ['Playfair Display', 'serif'], // Fuente para títulos
+      },
+    },
   },
   plugins: [],
 }
